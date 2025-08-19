4a141d5427c558144cb95c4eb2ec416f8826e017
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2025-08-19 15:42:21 +0300

    Update topic stat measurement scrıpt

--- meas_topic_stats.py
+++ meas_topic_stats.py
@@ -4,6 +4,7 @@ import importlib
 import logging
 import os
 import time
+from inspect import getmembers
 
 import message_filters
 import numpy as np
@@ -14,6 +15,12 @@ rospy.init_node('sf', anonymous=True)
 rospy.set_param('/rosgraph/log_level', logging.DEBUG)
 
 rospy.loginfo("Node initialized")
+
+
+# %% Utils
+
+def has_header_field(msg_class):
+    return any(name == 'header' for name, _ in getmembers(msg_class))
 
 
 # %% Topics
@@ -44,7 +51,48 @@ def import_msg_class(msg_type):
 ignored_topics = [
     '/rosout_agg',
     '/rosout',
-    '/clock',
+    # '/clock',
+    '/clicked_point',
+
+    '/initialpose',
+    '/move_base_simple/goal',
+    '/piksi/navsatfix_best_fix',
+    # '/jackal_velocity_controller/odom',
+    '/microstrain/imu/data',
+    '/microstrain/mag',
+
+    # '/velodyne_points',
+
+    # '/zed2i/zed_node/depth/depth_registered',
+    # '/zed2i/zed_node/left/image_rect_color',
+    # '/zed2i/zed_node/left/camera_info',
+    # '/zed2i/zed_node/imu/data',
+    # '/zed2i/zed_node/right/camera_info',
+    # '/zed2i/zed_node/depth/camera_info',
+    '/zed2i/zed_node/right/image_rect_color',
+    '/zed2i/zed_node/confidence/confidence_map',
+    '/zed2i/zed_node/imu/mag',
+    '/zed2i/zed_node/pose',
+
+    ## lidar upsample
+    '/transformed_odom',
+    '/transformed_point_cloud',
+    '/cumulative_point_cloud',
+    # '/cumulative_origin_point_cloud',
+
+    ## main
+    '/islam/vlp_filtered_pointcloud',
+    '/islam/pg_rgb',
+    # '/islam/pg_depth',
+    '/islam/zed_pointcloud',
+    '/islam/vlp_depth',
+    '/islam/pg_fused_pointcloud',\
+    '/islam/pg_odom',
+    '/islam/pg_camera_info',
+    '/islam/vlp_pts',
+    '/islam/vlp_odom',
+    '/islam/zed_original_pointcloud',
+    '/islam/vlp_debug_pointcloud',
 ]
 
 discovered_topics = get_all_topic_types()
@@ -56,6 +104,11 @@ for topic, msg_type in discovered_topics:
         continue
 
     msg_class = import_msg_class(msg_type)
+
+    if not has_header_field(msg_class):
+        rospy.logwarn(f"{topic}: [No header filed on topic]")
+        continue
+
     if msg_class:
         rospy.loginfo(f"{topic}: {msg_class}")
         topics.append((topic, msg_class))
@@ -71,6 +124,7 @@ csv_writer = csv.writer(log_file)
 csv_writer.writerow([
     'Timestamp',
     *[topic for topic, _ in topics],
+    'total_processing_time_ms',
     'Timestamp_Variance_sec',
 ])  # CSV Header
 
@@ -78,8 +132,13 @@ rospy.loginfo("CSV log file generated")
 
 
 # %%
+last_time = rospy.get_rostime().now()
+
 def synchronized_callback(*msgs):
-    rospy.loginfo("synchronized_callback")
+    global last_time
+    now = rospy.get_rostime().now()
+    rospy.loginfo(f"synchronized_callback: {(rospy.get_rostime().now() - last_time).to_sec()}")
+    last_time = now
 
     global csv_writer, log_file
 
@@ -90,7 +149,6 @@ def synchronized_callback(*msgs):
     for msg in msgs:
         timestamps.append(msg.header.stamp.to_sec())
 
-    # End timing for the entire zed_callback processing
     total_end_time = time.time()
     total_processing_time_ms = (total_end_time - total_start_time) * 1000
 
@@ -98,6 +156,7 @@ def synchronized_callback(*msgs):
     csv_writer.writerow([
         time.time(),  # System timestamp
         *timestamps,
+        total_processing_time_ms,
         np.var(timestamps),
     ])
     log_file.flush()  # Ensure data is written to disk immediately
